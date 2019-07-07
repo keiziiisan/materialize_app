5e1ecad51461fb4a83d964756e637ca7f1ddf707
oshikadota <oshi0824@gmail.com>
2019-07-07 10:14:07 +0900

    first commit

---
+++ js/materialize.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {});
+});
